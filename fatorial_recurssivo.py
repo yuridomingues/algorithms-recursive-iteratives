3343f91788b306d2946002d9c84128c801e8993d
Yuri Domingues <yuridomingues.contato@gmail.com>
2025-09-16 21:09:43 -0300

    Update fatorial_recurssivo.py

--- fatorial_recurssivo.py
+++ fatorial_recurssivo.py
@@ -12,3 +12,31 @@ if __name__ == "__main__":
     print(fatorial_recurssivo(int(input())))
     end = time.time()
     print("Tempo de execução:", end - start)
+
+
+def fibonacci_top_down(n):
+    if n == 1 or n == 0:
+        return n
+    
+    try:
+        return tabela[n]
+    except:
+        tabela[n] = fibonacci_top_down(n - 1) + fibonacci_top_down(n - 2)
+        return tabela[n]
+        
+def fibonacci_bottom_up(n):
+    tabela = {}
+    tabela[0] = 0
+    tabela[1] = 1
+    
+    for i in range(2, n + 1):
+        tabela[i] = tabela[i - 1] + tabela[i - 2]
+    
+if __name__ == "__main__":
+    tabela = {}
+    
+    n = [0, 5, 10, 77, 1000]
+    
+    for i in range(len(n)):
+        instancia = n[i]
+        print(fibonacci_top_down(n = instancia))
